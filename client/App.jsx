7ab05de835b085f51b67c930622b44820fdd540d
Joseph Oliver Comia <oliver.r.comia@gmail.com>
2020-05-12 10:42:02 -0400

    Use react-router-dom

--- client/App.jsx
+++ client/App.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import ReactRouter from "./utility/ReactRouter.jsx";
 
 const App = () => {
-  return <div>Display App</div>;
+  return (
+    <div>
+      Display App
+      <ReactRouter />
+    </div>
+  );
 };
 
 export default App;
