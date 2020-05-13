02f1024a815adb303bea6e0d6d51043a2fdb095e
David Josephs <davidjosephs12@gmail.com>
2020-05-13 13:13:09 -0400

    (fix) eslint to autosave and work with jest.

--- index.test.js
+++ index.test.js
@@ -1,13 +1,13 @@
 // FOR INITIAL TEST (think hello world)
-import "core-js/stable";
-import "regenerator-runtime/runtime";
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 
-describe("Google", () => {
+describe('Google', () => {
   beforeAll(async () => {
-    await page.goto("https://google.com");
+    await page.goto('https://google.com');
   });
 
   it('should display "google" text on page', async () => {
-    await expect(page).toMatch("google");
+    await expect(page).toMatch('google');
   });
 });
