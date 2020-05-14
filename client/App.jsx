c10299fcfa2761b74f53cdf9a1f676ce2ac1b2a0
Joseph Oliver Comia <oliver.r.comia@gmail.com>
2020-05-14 08:12:22 -0400

    Add ratings_reviews components

--- client/App.jsx
+++ client/App.jsx
@@ -1,15 +1,16 @@
-import React from "react";
-import ReactRouter from "./utility/ReactRouter.jsx";
-import DisplayRelatedItems from "./related_products/redux/containers/DisplayRelatedItems";
+import React from 'react';
+import ReactRouter from './utility/ReactRouter.jsx';
+import DisplayRelatedItems from './related_products/redux/containers/DisplayRelatedItems';
+import Reviews from './ratings_reviews/Reviews.jsx';
 
 const App = () => {
   return (
     <div>
       Display App
       <ReactRouter />
-
       <div>
         <DisplayRelatedItems />
+        <Reviews />
       </div>
     </div>
   );
