cba93b0c6eff65a9c04041d6c0c2ffc627c725bb
Joseph Oliver Comia <oliver.r.comia@gmail.com>
2020-05-20 15:59:23 -0400

    Minor update

--- client/App.jsx
+++ client/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import HomeContainer from './redux/containers/homeContainer.js';
 import { Container } from '@material-ui/core';
-import ReviewContainer from './redux/containers/reviewContainer.js';
 
 const App = () => {
   return (
     <Container>
       <HomeContainer />
-      {/* <ReviewContainer /> */}
     </Container>
   );
 };
