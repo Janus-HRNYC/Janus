aa948f4d3233ab11b51beb1071ac15ccd7157d48
Joseph Oliver Comia <oliver.r.comia@gmail.com>
2020-05-23 14:30:00 -0400

    Adding unit test for Star Rating

--- client/ratings_reviews/tests/reviews.spec.js
+++ client/ratings_reviews/tests/reviews.spec.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
 import TextFieldForm from '../form_components/TextFieldForm.jsx';
+import StarRating from '../StarRating.jsx';
 
 describe('Reviews: TextFieldForm', () => {
   test('it should display the TextField form', () => {
-    const wrapper = shallow(
+    const wrapper = mount(
       <TextFieldForm type={'summary'} value={'summary'} error={false} />
     );
-    expect(wrapper.exists()).toBe(true);
+    expect(wrapper.props().value).toEqual('summary');
+    wrapper.setProps({ value: 'body' });
+    expect(wrapper.props().value).toEqual('body');
+  });
+});
+
+describe('Reviews: Star Rating', () => {
+  test('it should display the Start Rating', () => {
+    const wrapper = mount(<StarRating star={5} size={'large'} update={true} />);
+    expect(wrapper.props().star).toEqual(5);
+    wrapper.setProps({ star: 1 });
+    expect(wrapper.props().star).toEqual(1);
   });
 });
