ca221ff2a06dd9e19bccbe1d08fbc7e8be1327d6
Joseph Oliver Comia <oliver.r.comia@gmail.com>
2020-05-23 14:57:20 -0400

    Added more component tests

--- client/ratings_reviews/ProductBreakdown.jsx
+++ client/ratings_reviews/ProductBreakdown.jsx
@@ -3,14 +3,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import { Typography, Slider, Grid } from '@material-ui/core';
 
 const ProductBreakdown = ({ label, value, marks }) => {
-  // const useStyles = makeStyles({
-  //   root: {
-  //     width: 300,
-  //   },
-  // });
-
-  // const classes = useStyles();
-
   return (
     <Grid item md={10}>
       <Typography variant='body1' gutterBottom>
--- client/ratings_reviews/tests/reviews.spec.js
+++ client/ratings_reviews/tests/reviews.spec.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
 import TextFieldForm from '../form_components/TextFieldForm.jsx';
+import Recommend from '../form_components/Recommend.jsx';
+import RatingBreakdown from '../RatingBreakdown.jsx';
 import StarRating from '../StarRating.jsx';
+import RatingSummary from '../RatingSummary.jsx';
 
 describe('Reviews: TextFieldForm', () => {
-  test('it should display the TextField form', () => {
+  test('it should display the TextField component', () => {
     const wrapper = mount(
       <TextFieldForm type={'summary'} value={'summary'} error={false} />
     );
@@ -15,10 +18,39 @@ describe('Reviews: TextFieldForm', () => {
 });
 
 describe('Reviews: Star Rating', () => {
-  test('it should display the Start Rating', () => {
+  test('it should display the Start Rating component', () => {
     const wrapper = mount(<StarRating star={5} size={'large'} update={true} />);
     expect(wrapper.props().star).toEqual(5);
     wrapper.setProps({ star: 1 });
     expect(wrapper.props().star).toEqual(1);
+  });
+});
+
+describe('Reviews: Recommend', () => {
+  test('it should display the Recommend component', () => {
+    const wrapper = mount(<Recommend value={5} />);
+    expect(wrapper.props().value).toEqual(5);
+    wrapper.setProps({ value: 2 });
+    expect(wrapper.props().value).toEqual(2);
+  });
+});
+
+describe('Reviews: Rating Breakdown', () => {
+  test('it should display the RatingBreakdown component', () => {
+    const wrapper = mount(<RatingBreakdown rating={5} ratingValue={50} />);
+    expect(wrapper.props().rating).toEqual(5);
+    wrapper.setProps({ rating: 1 });
+    expect(wrapper.props().rating).toEqual(1);
+  });
+});
+
+describe('Reviews: Rating Summary', () => {
+  test('it should display the Rating Summary component', () => {
+    const wrapper = mount(
+      <RatingSummary ratings={{ 1: '1' }} recommended={{ 1: '1' }} />
+    );
+    expect(wrapper.props().ratings).toEqual({ 1: '1' });
+    wrapper.setProps({ ratings: { 2: '1' } });
+    expect(wrapper.props().ratings).toEqual({ 2: '1' });
   });
 });
